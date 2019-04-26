e562bc68f9d134f307cd7c0a926b640d8f13f69c
Raunak <raunakdaga@gmail.com>
2019-04-26 14:35:09 -0400

    Files

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, static_folder='static')
 
+names = ['Uncle', 'Mom']
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
-
+    if request.method == POST:
+        variable = request.form['information']
+        return render_template('index.html', information='I love flask.', names=names, variabl=variable)
+    return render_template('index.html', information='I love Flask.')
 if __name__ == "main":
     app.run(debug=True)
